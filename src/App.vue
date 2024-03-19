978a468729c9df3026253ce015dcd4072439e847
Cristian <cristianfierrosilva@gmail.com>
2024-03-19 17:41:12 -0500

    Add new files and initial configuration

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+</script>
+
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap");
+* {
+  font-family: Poppins, Helvetica, Arial, sans-serif;
+}
+</style>
