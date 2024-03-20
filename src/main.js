4b5a0f9465b6b9f3ae277dd3f7217b294dc93453
Cristian <cristianfierrosilva@gmail.com>
2024-03-19 19:03:32 -0500

    Add PrimeVue to app configuration

--- src/main.js
+++ src/main.js
@@ -2,11 +2,12 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import store from "@/store";
-import router from "./router/index.js"
+import router from "./router/index.js";
+import PrimeVue from "primevue/config";
 
 const app = createApp(App);
 
 app.use(store);
 app.use(router);
+app.use(PrimeVue);
 app.mount("#app");
-
