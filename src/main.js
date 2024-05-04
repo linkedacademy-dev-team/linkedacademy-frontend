8374ee39178e8539cfa1ad2142806d5252534956
Cristian <cristianfierrosilva@gmail.com>
2024-05-03 20:07:59 -0500

    Add @vue-leaflet/vue-leaflet package and components to main.js

--- src/main.js
+++ src/main.js
@@ -4,10 +4,16 @@ import App from "./App.vue";
 import store from "@/store";
 import router from "./router/index.js";
 import PrimeVue from "primevue/config";
+import { LMap, LTileLayer, LMarker } from "@vue-leaflet/vue-leaflet";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
 
 const app = createApp(App);
 
 app.use(store);
+app.component("l-map", LMap);
+app.component("l-tite-layer", LTileLayer);
+app.component("l-marker", LMarker);
 app.use(router);
 app.use(PrimeVue);
 app.mount("#app");
