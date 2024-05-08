ae6f5931d1ccf96c2a55787d20c376eae84129b3
Cristian <cristianfierrosilva@gmail.com>
2024-05-08 17:58:14 -0500

    Add fade animation to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        fade: 'fadeOut 0.5s ease-in-out',
+      },
+    },
   },
   plugins: [require("@tailwindcss/forms")],
 };
