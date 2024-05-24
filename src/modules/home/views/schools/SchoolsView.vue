c31f6079a88fb661237b3fd3b6d367eba2cea011
Cristian <cristianfierrosilva@gmail.com>
2024-05-24 12:34:05 -0500

    feat: Add EditSchedules.vue component for editing schedules

--- src/modules/home/views/schools/SchoolsView.vue
+++ src/modules/home/views/schools/SchoolsView.vue
@@ -1,86 +1,81 @@
 <template>
-    <div class="bg-gray-900 rounded">
-      <div class="mx-auto max-w-7xl">
-        <div class="bg-gray-900 py-10">
-          <div class="px-4 sm:px-6 lg:px-8">
-            <div class="sm:flex sm:items-center">
-              <div class="sm:flex-auto">
-                <h1 class="text-base font-semibold leading-6 text-white">
-                  Escuelas
-                </h1>
-                <p class="mt-2 text-sm text-gray-300">Una lista de escuelas.</p>
-              </div>
-              <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
-                <button
-                  type="button"
-                  class="block rounded-md bg-indigo-500 px-3 py-2 text-center text-sm font-semibold text-white hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
-                >
-                  Añadir escuela
-                </button>
-              </div>
+  <div class="bg-gray-900 rounded">
+    <div class="mx-auto max-w-7xl">
+      <div class="bg-gray-900 py-10">
+        <div class="px-4 sm:px-6 lg:px-8">
+          <div class="sm:flex sm:items-center">
+            <div class="sm:flex-auto">
+              <h1 class="text-base font-semibold leading-6 text-white">
+                Discapacidades
+              </h1>
+              <p class="mt-2 text-sm text-gray-300">
+                Una lista de discapacidades.
+              </p>
             </div>
-            <div class="mt-8 flow-root">
-              <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                <div
-                  class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8"
-                >
-                  <table class="min-w-full divide-y divide-gray-700">
-                    <thead>
-                      <tr>
-                        <th
-                          scope="col"
-                          class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-white sm:pl-0"
-                        >
-                          Nombre
-                        </th>
-                        <th
-                          scope="col"
-                          class="px-3 py-3.5 text-left text-sm font-semibold text-white"
-                        >
-                          Title
-                        </th>
-                        <th
-                          scope="col"
-                          class="px-3 py-3.5 text-left text-sm font-semibold text-white"
-                        >
-                          Email
-                        </th>
-                        <th
-                          scope="col"
-                          class="px-3 py-3.5 text-left text-sm font-semibold text-white"
-                        >
-                          Role
-                        </th>
-                        <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-0">
-                          <span class="sr-only">Edit</span>
-                        </th>
-                      </tr>
-                    </thead>
-                    <tbody class="divide-y divide-gray-800">
-                      <tr v-for="person in people" :key="person.email">
-                        <td
-                          class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-white sm:pl-0"
-                        >
-                          {{ person.name }}
-                        </td>
-                        <td
-                          class="whitespace-nowrap px-3 py-4 text-sm text-gray-300"
-                        >
-                          {{ person.title }}
-                        </td>
-                        <td
-                          class="whitespace-nowrap px-3 py-4 text-sm text-gray-300"
-                        >
-                          {{ person.email }}
-                        </td>
-                        <td
-                          class="whitespace-nowrap px-3 py-4 text-sm text-gray-300"
-                        >
-                          {{ person.role }}
-                        </td>
-                        <td
-                          class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0"
-                        >
+            <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
+              <button
+                type="button"
+                class="block rounded-md bg-indigo-500 px-3 py-2 text-center text-sm font-semibold text-white hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+              >
+                Añadir discapacidad
+              </button>
+            </div>
+          </div>
+          <div class="mt-8 flow-root">
+            <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+              <div
+                class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8"
+              >
+                <table class="min-w-full divide-y divide-gray-700">
+                  <thead>
+                    <tr>
+                      <th
+                        scope="col"
+                        class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-white sm:pl-0"
+                      >
+                        Nombre
+                      </th>
+                      <th
+                        scope="col"
+                        class="px-3 py-3.5 text-left text-sm font-semibold text-white"
+                      >
+                        Creado
+                      </th>
+                      <th
+                        scope="col"
+                        class="px-3 py-3.5 text-left text-sm font-semibold text-white"
+                      >
+                        Modificado
+                      </th>
+
+                      <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-0">
+                        <span class="sr-only">Edit</span>
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody class="divide-y divide-gray-800">
+                    <tr
+                      v-for="educationalLevel in educationalLevelsData"
+                      :key="educationalLevel.id"
+                    >
+                      <td
+                        class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-white sm:pl-0"
+                      >
+                        {{ educationalLevel?.name }}
+                      </td>
+                      <td
+                        class="whitespace-nowrap px-3 py-4 text-sm text-gray-300"
+                      >
+                        {{ educationalLevel?.createdAt }}
+                      </td>
+                      <td
+                        class="whitespace-nowrap px-3 py-4 text-sm text-gray-300"
+                      >
+                        {{ educationalLevel.updatedAt }}
+                      </td>
+                      <td
+                        class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0"
+                      >
                         <div class="text-indigo-400 hover:text-indigo-300">
                           <span class="flex flex-grow space-x-4"
                             ><button
@@ -93,30 +88,30 @@
                             </button>
                           </span>
                         </div>
-                          >
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </div>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  </template>
-  
-  <script setup>
-  import { PencilSquareIcon, TrashIcon } from "@heroicons/vue/20/solid";
-  const people = [
-    {
-      name: "Lindsay Walton",
-      title: "Front-end Developer",
-      email: "lindsay.walton@example.com",
-      role: "Member",
-    },
-    // More people...
-  ];
-  </script>
-  
+  </div>
+</template>
+
+<script setup>
+import { ref, computed, onMounted } from "vue";
+import { useStore } from "vuex";
+import { PencilSquareIcon, TrashIcon } from "@heroicons/vue/20/solid";
+
+const store = useStore();
+
+
+const educationalLevelsData = computed(() => store.state.education_levels);
+
+onMounted(async () => {
+  await store.dispatch("GET_EDUCATION_LEVELS");
+});
+</script>
