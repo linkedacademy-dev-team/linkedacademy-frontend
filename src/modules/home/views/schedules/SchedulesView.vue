aad82698a221560bce5365585efbf345d9622545
Cristian <cristianfierrosilva@gmail.com>
2024-05-24 12:33:45 -0500

    feat: Update SchedulesView table headers and data bindings
    - Update the table headers in SchedulesView.vue to display "Nombre" instead of "Name" and "Creado" instead of "Title".
    - Modify the table data bindings to display the session name, creation date, and modification date.
    - Refactor the v-for directive to iterate over the "sessionsData" array instead of "people".
    - Remove unnecessary code related to editing specialities.
    
    Closes #123

--- src/modules/home/views/schedules/SchedulesView.vue
+++ src/modules/home/views/schedules/SchedulesView.vue
@@ -12,6 +12,7 @@
             </div>
             <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
               <button
+                @click="openAddSchedules"
                 type="button"
                 class="block rounded-md bg-indigo-500 px-3 py-2 text-center text-sm font-semibold text-white hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
               >
@@ -31,64 +32,52 @@
                         scope="col"
                         class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-white sm:pl-0"
                       >
-                        Name
+                        Nombre
                       </th>
                       <th
                         scope="col"
                         class="px-3 py-3.5 text-left text-sm font-semibold text-white"
                       >
-                        Title
+                        Creado
                       </th>
                       <th
                         scope="col"
                         class="px-3 py-3.5 text-left text-sm font-semibold text-white"
                       >
-                        Email
+                        Modificado
                       </th>
-                      <th
-                        scope="col"
-                        class="px-3 py-3.5 text-left text-sm font-semibold text-white"
-                      >
-                        Role
-                      </th>
+
                       <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-0">
                         <span class="sr-only">Edit</span>
                       </th>
                     </tr>
                   </thead>
                   <tbody class="divide-y divide-gray-800">
-                    <tr v-for="person in people" :key="person.email">
+                    <tr v-for="sessions in sessionsData" :key="sessions.id">
                       <td
                         class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-white sm:pl-0"
                       >
-                        {{ person.name }}
+                        {{ sessions?.name }}
                       </td>
                       <td
                         class="whitespace-nowrap px-3 py-4 text-sm text-gray-300"
                       >
-                        {{ person.title }}
+                        {{ sessions?.createdAt }}
                       </td>
                       <td
                         class="whitespace-nowrap px-3 py-4 text-sm text-gray-300"
                       >
-                        {{ person.email }}
-                      </td>
-                      <td
-                        class="whitespace-nowrap px-3 py-4 text-sm text-gray-300"
-                      >
-                        {{ person.role }}
+                        {{ sessions.updatedAt }}
                       </td>
                       <td
                         class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0"
                       >
-                      <div class="text-indigo-400 hover:text-indigo-300">
-                          <span class="flex flex-grow space-x-4"
-                            ><button
-                              @click="openEditSpecialities(specialities)"
-                            >
+                        <div class="text-indigo-400 ">
+                          <span class="flex flex-grow   space-x-4"
+                            ><button class="hover:text-indigo-300" @click="openEditSchedules(sessions)">
                               <PencilSquareIcon class="w-6 h-6" />
                             </button>
-                            <button @click="deleteSpecialitie(specialities.id)">
+                            <button class="hover:text-indigo-300" @click="deleteSchedules(sessions.id)">
                               <TrashIcon class="w-6 h-6" />
                             </button>
                           </span>
@@ -105,17 +94,56 @@
       </div>
     </div>
   </div>
+  <AddSchedules :open="addSchedules" @close-add="closeAddSchedules" />
+  <EditSchedules
+    :schedules="schedulesSelected"
+    :open="editSchedules"
+    @close-edit="closeEditSchedules"
+  />
 </template>
 
 <script setup>
+import { ref, computed, onMounted } from "vue";
+import { useStore } from "vuex";
 import { PencilSquareIcon, TrashIcon } from "@heroicons/vue/20/solid";
-const people = [
-  {
-    name: "Lindsay Walton",
-    title: "Front-end Developer",
-    email: "lindsay.walton@example.com",
-    role: "Member",
-  },
-  // More people...
-];
+import AddSchedules from "./common/addSchedules.vue";
+import EditSchedules from "./common/editSchedules.vue";
+
+const store = useStore();
+
+const sessionsData = computed(() => store.state.sessions);
+
+const addSchedules = ref(false);
+const editSchedules = ref(false);
+const schedulesSelected = ref(null);
+
+const openEditSchedules = (schedules) => {
+  editSchedules.value = true;
+  schedulesSelected.value = schedules;
+};
+
+const closeEditSchedules = () => {
+  editSchedules.value = false;
+};
+
+const deleteSchedules = async (id) => {
+  try {
+    await store.dispatch("DELETE_SESSION", id);
+    await store.dispatch("GET_SESSIONS");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const openAddSchedules = () => {
+  addSchedules.value = true;
+};
+
+const closeAddSchedules = () => {
+  addSchedules.value = false;
+};
+
+onMounted(async () => {
+  await store.dispatch("GET_SESSIONS");
+});
 </script>
