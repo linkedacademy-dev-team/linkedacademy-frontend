08184825ce9732b63e6b34a6567e08f66e57f2a2
Cristian <cristianfierrosilva@gmail.com>
2024-05-24 12:33:28 -0500

    feat: Update DisabilitiesView layout and data rendering

--- src/modules/home/views/languages/LanguagesView.vue
+++ src/modules/home/views/languages/LanguagesView.vue
@@ -6,16 +6,18 @@
           <div class="sm:flex sm:items-center">
             <div class="sm:flex-auto">
               <h1 class="text-base font-semibold leading-6 text-white">
-                Idiomas
+                Discapacidades
               </h1>
-              <p class="mt-2 text-sm text-gray-300">Una lista de lenguajes</p>
+              <p class="mt-2 text-sm text-gray-300">
+                Una lista de discapacidades.
+              </p>
             </div>
             <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
               <button
                 type="button"
                 class="block rounded-md bg-indigo-500 px-3 py-2 text-center text-sm font-semibold text-white hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
               >
-                Añadir idioma
+                Añadir discapacidad
               </button>
             </div>
           </div>
@@ -31,57 +33,50 @@
                         scope="col"
                         class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-white sm:pl-0"
                       >
-                        Name
+                        Nombre
                       </th>
                       <th
                         scope="col"
                         class="px-3 py-3.5 text-left text-sm font-semibold text-white"
                       >
-                        Title
+                        Creado
                       </th>
                       <th
                         scope="col"
                         class="px-3 py-3.5 text-left text-sm font-semibold text-white"
                       >
-                        Email
+                        Modificado
                       </th>
-                      <th
-                        scope="col"
-                        class="px-3 py-3.5 text-left text-sm font-semibold text-white"
-                      >
-                        Role
-                      </th>
+
                       <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-0">
                         <span class="sr-only">Edit</span>
                       </th>
                     </tr>
                   </thead>
                   <tbody class="divide-y divide-gray-800">
-                    <tr v-for="person in people" :key="person.email">
+                    <tr
+                      v-for="language in languagesData"
+                      :key="language.id"
+                    >
                       <td
                         class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-white sm:pl-0"
                       >
-                        {{ person.name }}
+                        {{ language?.name }}
                       </td>
                       <td
                         class="whitespace-nowrap px-3 py-4 text-sm text-gray-300"
                       >
-                        {{ person.title }}
+                        {{ language?.createdAt }}
                       </td>
                       <td
                         class="whitespace-nowrap px-3 py-4 text-sm text-gray-300"
                       >
-                        {{ person.email }}
-                      </td>
-                      <td
-                        class="whitespace-nowrap px-3 py-4 text-sm text-gray-300"
-                      >
-                        {{ person.role }}
+                        {{ language.updatedAt }}
                       </td>
                       <td
                         class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0"
                       >
-                      <div class="text-indigo-400 hover:text-indigo-300">
+                        <div class="text-indigo-400 hover:text-indigo-300">
                           <span class="flex flex-grow space-x-4"
                             ><button
                               @click="openEditSpecialities(specialities)"
@@ -93,7 +88,6 @@
                             </button>
                           </span>
                         </div>
-                        >
                       </td>
                     </tr>
                   </tbody>
@@ -108,14 +102,16 @@
 </template>
 
 <script setup>
+import { ref, computed, onMounted } from "vue";
+import { useStore } from "vuex";
 import { PencilSquareIcon, TrashIcon } from "@heroicons/vue/20/solid";
-const people = [
-  {
-    name: "Lindsay Walton",
-    title: "Front-end Developer",
-    email: "lindsay.walton@example.com",
-    role: "Member",
-  },
-  // More people...
-];
+
+const store = useStore();
+
+
+const languagesData = computed(() => store.state.languages);
+
+onMounted(async () => {
+  await store.dispatch("GET_LANGUAGES");
+});
 </script>
